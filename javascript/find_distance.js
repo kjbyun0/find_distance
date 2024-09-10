cc3cc266f705c737b0d22ccbef6e96f377fc311b
KI JONG BYUN <kjbyun0@gmail.com>
2024-09-09 19:28:51 -0500

    Add my solution

--- javascript/find_distance.js
+++ javascript/find_distance.js
@@ -1,6 +1,59 @@
+// function findDistance(graph, vertexA, vertexB) {
+//   // type your code here
+//   class QueueItem {
+//     constructor(vertex, distance = 0) {
+//       this.vertex = vertex;
+//       this.distance = distance;
+//     }
+//   }
+
+//   let front = 0, end = -1;
+//   const queue = [];
+//   queue.push(new QueueItem(vertexA, 0));
+//   end++;
+//   const visited = new Set();
+
+//   while (front <= end) {
+//     const qi = queue[front++];
+    
+//     visited.add(qi.vertex);
+//     for (const vertex of graph[qi.vertex]) {
+//       if (vertex === vertexB)
+//         return qi.distance + 1;
+
+//       if (!visited.has(vertex)) {
+//         queue.push(new QueueItem(vertex, qi.distance + 1));
+//         end++;
+//       }
+//     }
+//   }
+//   return -1;
+// }
+
+
 function findDistance(graph, vertexA, vertexB) {
   // type your code here
+  const queue = [[vertexA, 0]];
+  let front = 0, end = 0;
+  const visited = new Set();
+
+  while (front <= end) {
+    const [curVertex, distance] = queue[front++];
+    visited.add(curVertex);
+
+    for (const adjVertex of graph[curVertex]) {
+      if (adjVertex === vertexB)
+        return distance + 1;
+
+      if (!visited.has(adjVertex)) {
+        queue.push([adjVertex, distance+1]);
+        end++;
+      }
+    }
+  }
+  return -1;
 }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -19,6 +72,14 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log(findDistance(graph, "dave", "carl"));
+
+  console.log("");
+  console.log("Expecting: -1");
+  console.log(findDistance(graph, "jan", "jan"));
+
+  console.log("");
+  console.log("Expecting: 1");
+  console.log(findDistance(graph, "jan", "jambaby"));
 }
 
 module.exports = findDistance;
